e7d896483dd4046b38465fa06e8ff5e0ecacea4d
aubert-co <blacket021@gmail.com>
2026-06-29 23:05:26 -0300

    feat: add create user DTO

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -2,8 +2,17 @@ export class CreateUserDto {
   username!: string;
   email!: string;
   password!:string;
-  biog:string;
+  bio:string;
   status:string;
-
+  nickname!:string
 }
 
+export type   CreateUserInput = {
+  username:string,
+  email:string,
+  password:string,
+  bio?:string,
+  status?:string,
+  avatarFile?:Express.Multer.File
+  nickname:string
+}
